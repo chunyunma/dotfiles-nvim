702aa31dec4eaffeaca11b433dccff307d417563
chunyunma <chunyunma@cmail.carleton.ca>
2023-06-07 16:09:58 -0400

    Updated based on the latest README of treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,14 +1,24 @@
+-- updated based on the latest README of treesitter
+
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
 	-- "latex", syntax highlight from latex treesitter does not work with vimtex conceal
 	"r",
 	"lua",
+  "c",
+  "markdown", -- seems to conflict with pandoc plugin for highlight
+  "vim",
+  "vimdoc",
+  "query",
 	"bash"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  sync_install = false,
+  -- auto_install = true, -- disabled so that it won't automatically install markdown parser
   ignore_install = { }, -- List of parsers to ignore installing
   highlight = {
 	enable = true,  -- false will disable the whole extension
 	disable = { },  -- list of language that will be disabled
   },
+  additional_vim_regex_highlighting = false,
   rainbow = {
 	enable = true,
 	extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
